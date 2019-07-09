21159dbe8960f1b1a68519a94d4acc61025c4713
Diane Looney <diane@dianelooney.com>
2019-07-08 21:03:08 -0400

    Update tests, ignore datarace test

--- ferry_test.go
+++ ferry_test.go
@@ -2,7 +2,6 @@ package ferry_test
 
 import (
 	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
 
@@ -10,107 +9,42 @@ import (
 )
 
 func TestItBlocks(t *testing.T) {
-	var order []int
 	b := New()
 	go func() {
-		time.Sleep(250 * time.Millisecond)
-		order = append(order, 1)
-		b.Done()
+		b.Wait()
+		t.Error("It did not block")
 	}()
-	b.Wait()
-	order = append(order, 2)
-	if order[0] != 1 || order[1] != 2 {
-		t.Error("It did not block in the proper order")
-	}
+	time.Sleep(10 * time.Millisecond)
 }
 
 func TestItBlocksMany(t *testing.T) {
 	const n = 1000
-	done := 0
-	mtx := sync.Mutex{}
-
 	b := New()
+	wg := sync.WaitGroup{}
+	wg.Add(n)
 	for i := 0; i < n; i++ {
 		go func() {
 			b.Wait()
-
-			mtx.Lock()
-			defer mtx.Unlock()
-			done++
+			wg.Done()
 		}()
 	}
-	time.Sleep(250 * time.Millisecond)
-	if done != 0 {
-		t.Errorf("It did not block all goroutines")
-	}
+	time.Sleep(10 * time.Millisecond)
 	b.Done()
-	time.Sleep(250 * time.Millisecond)
-
-	mtx.Lock()
-	defer mtx.Unlock()
-
-	if done != n {
-		t.Errorf("It did not unblock all goroutines")
-	}
+	wg.Wait()
 }
 
-func TestItBlocksWhileUnblocking(t *testing.T) {
-	const n = 1000
-	fail := false
-	b := New()
-	for i := 0; i < n; i++ {
-		go func() {
-			for {
-				b.Wait()
-				b.Wait()
-				fail = true
-			}
-		}()
-	}
-
-	done := false
-	mtx := sync.Mutex{}
-	go func() {
-		b.Done()
-		mtx.Lock()
-		defer mtx.Unlock()
-		done = true
-	}()
-	time.Sleep(250 * time.Millisecond)
-
-	mtx.Lock()
-	defer mtx.Unlock()
-
-	if !done {
-		t.Errorf("It did not return")
-	}
-
-	if fail {
-		t.Errorf("It unlocked the same goroutine more than once")
-	}
-}
-
-func TestMultipleUnlockCalls(t *testing.T) {
+func TestItUnblocksGoroutinesOnce(t *testing.T) {
 	const n = 1000
 	b := New()
-	var count int32
 	for i := 0; i < n; i++ {
 		go func() {
 			b.Wait()
 			b.Wait()
-			atomic.AddInt32(&count, 1)
+			t.Errorf("It unlocked the same goroutine more than once")
 		}()
 	}
-
-	time.Sleep(250 * time.Millisecond)
 	b.Done()
-	time.Sleep(250 * time.Millisecond)
-	b.Done()
-	time.Sleep(250 * time.Millisecond)
-
-	if n != atomic.LoadInt32(&count) {
-		t.Errorf("It did not unblock all goroutines twice, expected '%v' got '%v'", n, count)
-	}
+	time.Sleep(10 * time.Millisecond)
 }
 
 func BenchmarkDone(b *testing.B) {
